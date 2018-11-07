5689bb3c4a8e5b2bb69209e2e69538bdb20c7650
ReadyDoMe <unitecomrades777@gmail.com>
2018-11-07 12:48:18 -0600

    init - implemented landing page

--- src/Components/Login/styles.js
+++ src/Components/Login/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const LoginContainer = styled.div`
-    background-color: yellow;
+    background-color: ${props => props.theme.colors.activeOrange};
 `;
