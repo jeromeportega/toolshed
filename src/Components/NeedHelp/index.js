5f168b60ae4f6b4dc261d58bc40d546a8be3586a
Jerome Ortega <jeromeortega@Jeromes-MacBook-Pro-2.local>
2018-11-07 13:58:52 -0600

    Added auto-suggester, works as needed

--- src/Components/NeedHelp/index.js
+++ src/Components/NeedHelp/index.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { NeedHelpContainer } from './styles';
 import Input from '../Input';
 
-const categories = [
-    'Electrician',
-    'Plumber',
-    'Drywall Expert',
-    'Painter',
+import Autosuggest from 'react-autosuggest';
+
+const suggestions = [
+    {
+        name: 'Electrician',
+    },
 ];
 
 class NeedHelp extends Component {
@@ -17,6 +18,7 @@ class NeedHelp extends Component {
             name: '',
             email: '',
             category: '',
+            value: '',
         }
     }
 
@@ -27,16 +29,59 @@ class NeedHelp extends Component {
         else console.log("There was an error in the NeedHelp component.");
     }
 
+    onSuggestionChange = (event, { newValue }) => {
+        this.setState({ value: newValue });
+    }
+
+    onSuggestionsFetchRequested = ({ value }) => {
+        this.setState({
+        suggestions: getSuggestions(value)
+        });
+    };
+
+    onSuggestionsClearRequested = () => {
+        this.setState({
+        suggestions: []
+        });
+    };
+
     render() {
-        const { name, email } = this.state;
+        const { name, email, value } = this.state;
+
+        const inputProps = {
+            placeholder: 'What do you need help with?',
+            value,
+            onChange: this.onSuggestionChange,
+          };
 
         return (
             <NeedHelpContainer>
                 <Input name="name" value={name} placeholder="Your Name" onChange={(e) => this.handleChange(e)} />
                 <Input name="email" value={email} placeholder="E-Mail Address" onChange={(e) => this.handleChange(e)} />
+                <Autosuggest
+                    suggestions={suggestions}
+                    getSuggestionValue={getSuggestionValue}
+                    renderSuggestion={renderSuggestion}
+                    inputProps={inputProps}
+                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                />
             </NeedHelpContainer>
         );
     }
 }
+
+const getSuggestionValue = suggestion => suggestion.name;
+const renderSuggestion = suggestion => (
+    <div>
+      {suggestion.name}
+    </div>
+);
+
+const getSuggestions = value => {
+    const inputValue = value.trim().toLowerCase();
+  
+    return inputValue;
+};
 
 export default NeedHelp;
--- src/Components/NeedHelp/styles.js
+++ src/Components/NeedHelp/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const NeedHelpContainer = styled.div`
-    background-color: black;
+    background-color: white;
 `;
