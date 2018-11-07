aee6ba30513840dbc9aef5bf027699514a8c90ad
ReadyDoMe <unitecomrades777@gmail.com>
2018-11-07 13:25:44 -0600

    init - styled input component

--- src/Components/Input/styles.js
+++ src/Components/Input/styles.js
@@ -2,4 +2,12 @@ import styled from 'styled-components';
 
 export const StyledInput = styled.input`
     width: 100%;
+    border-bottom: 2px solid black;
+    &:focus {
+        outline: none;
+        border-bottom: 2px solid ${props => props.theme.colors.activeOrange};
+    }
+    &:active {
+        outline: none;
+    }
 `;
