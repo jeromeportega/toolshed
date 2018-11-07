b87161899af30273880cc9e4b00864cb686106e4
Jerome Ortega <jeromeortega@Jeromes-MacBook-Pro-2.local>
2018-11-07 14:21:20 -0600

    Added server logic and sockets

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,11 @@
 var app = require('http').createServer();
+var io = module.exports.io = require('socket.io')(app);
 
 const PORT = process.env.PORT || 8000;
+
+const SocketManager = require('./SocketManager');
+
+io.on('connection', SocketManager);
 
 app.listen(PORT, () => {
   console.log("Connected to port:" + PORT);
