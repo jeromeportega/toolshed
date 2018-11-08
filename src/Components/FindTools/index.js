19524cb3e9cc14e90c435e7479348e7387acff4a
ReadyDoMe <unitecomrades777@gmail.com>
2018-11-07 21:55:41 -0600

    updated find-tools

--- src/Components/FindTools/index.js
+++ src/Components/FindTools/index.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import { Container } from './styles';
+import { Container, StyledBtn } from './styles';
+import searchImage from './images/search.png';
 
 class FindTools extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      searchString: null,
+      searchString: '',
+      btnClicked: false,
     }
   };
 
+  updateSearchString = (e) => {
+    this.setState({
+      searchString: e.currentTarget.value,
+    })
+  }
+
+  updateBtnClicked = () => {
+    this.setState({
+      btnClicked: true,
+    })
+  }
+
   render() {
+    const { searchString, btnClicked } = this.state;
     return(
-      <Container>
+      <Container searchString={searchString}>
+        {console.log(searchString)}
         <span className="title">
           A low-cost alternative to renting tools.
         </span>
@@ -20,7 +36,17 @@ class FindTools extends Component {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </span>
-        <input className="search" name="" value="searchString" />
+        <div className="search-container">
+          <img className="search-image" src={searchImage} alt="search"/>
+          <input className="search" name="searchString" value={searchString} placeholder="What are you looking for?" onChange={this.updateSearchString} />
+          <StyledBtn onClick={this.updateBtnClicked}>find tools</StyledBtn>
+          {!btnClicked
+            ?
+              <span>Button ! Clicked</span>
+            :
+            <span>Button Clicked!!!!</span>
+          }
+        </div>
       </Container>
     )
   };
--- src/Components/FindTools/styles.js
+++ src/Components/FindTools/styles.js
@@ -1,7 +1,13 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import screwsImage from './images/screws.jpg';
 
 export const Container = styled.div`
   height: calc(100vh - 150px);
+  background-image: url(${screwsImage});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  padding-top: 60px;
 
   .title {
     width: 100%;
@@ -19,5 +25,63 @@ export const Container = styled.div`
     text-align: center;
     font-weight: 200;
     margin-bottom: 40px;
+  }
+
+  .search-container {
+    margin: 0 auto;
+    max-width: 580px;
+    max-height: 55px;
+    position: relative;
+  }
+
+  .search {
+    width: 100%;
+    height: 100%;
+    font-size: 24px;
+    padding: 10px;
+    margin-bottom: 40px;
+    ${props => !props.searchString && css`
+      font-style: italic;
+    `}
+    &:focus {
+      outline: none;
+    }
+  }
+
+  .search-image {
+    width: 20px;
+    height: 20px;
+    position: absolute;
+    top: 18px;
+    right: 10px;
+  }
+`;
+
+export const StyledBtn = styled.button`
+  cursor: pointer;
+  width: 288px;
+  height: 80px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  color: white;
+  background-color: ${props => props.theme.colors.activeOrange};
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  z-index: 1;
+  border: none;
+  text-decoration: none;
+  &:hover {
+      color: black;
+      background-color: white;
+      border: 2px solid black;
+  }
+  &:active {
+      outline: none;
+  }
+  &:focus{
+      outline: none;
   }
 `;
